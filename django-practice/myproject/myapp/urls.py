2f26557258b49d754d5d052fb4508fcdff0080e1
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-15 13:39:33 -0700

    A string path with a string converter

--- django-practice/myproject/myapp/urls.py
+++ django-practice/myproject/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('drinks/<str:drink_name>', views.drinks, name = "drink_name"), 
-    ]
+    path('drinks/<str:drink_name>')
+]
