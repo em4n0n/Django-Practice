a91bf4b3563fe13f2638e32dea0213b63a552ef4
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-16 22:21:40 -0700

    Update to app urls

--- django-practice/myproject/myapp/urls.py
+++ django-practice/myproject/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('drinks/<str:drink_name>')
+    path('drinks/<str:drink_name>', views.drinks, name="drink_name"),
+    path('login/', views.login, name='login'),
 ]
