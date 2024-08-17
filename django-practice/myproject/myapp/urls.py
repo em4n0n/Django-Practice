9b13fff6fa66a88d67bfb757b730f6a442969b79
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-17 12:42:11 -0700

    Standard url pattern

--- django-practice/myproject/myapp/urls.py
+++ django-practice/myproject/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('drinks/<str:drink_name>', views.drinks, name="drink_name"),
     path('login/', views.login, name='login'),
+    path('', views.index, name='index'),
 ]
