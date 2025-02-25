0c5a04d7ba2fcce053bd7d3ace331d0554df7336
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-02-24 20:55:29 -0800

    Update the form object if the method is POST

--- C7M3L2 Lab/myproject/myapp/views.py
+++ C7M3L2 Lab/myproject/myapp/views.py
@@ -6,3 +6,6 @@ from django.http import JsonResponse
 # Create your views here.
 def form_view(request):
     form = CommentForm() # create a form object
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
